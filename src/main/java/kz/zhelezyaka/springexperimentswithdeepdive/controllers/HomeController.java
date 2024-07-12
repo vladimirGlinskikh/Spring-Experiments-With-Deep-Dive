6d91c3ae7be096ec800f196657799d27e2ea337a
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-07-12 17:35:38 +0300

    Test for controller home page

--- src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controllers/HomeController.java
+++ src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controllers/HomeController.java
@@ -1,9 +1,9 @@
 package kz.zhelezyaka.springexperimentswithdeepdive.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/v1/home")
---
+++ src/test/java/kz/zhelezyaka/springexperimentswithdeepdive/controllers/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package kz.zhelezyaka.springexperimentswithdeepdive.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/v1/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Devtool in Action")));
+    }
+}
