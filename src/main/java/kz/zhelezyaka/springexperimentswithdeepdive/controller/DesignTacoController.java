6328bb68220acbe17b08876e3c945b3c6fca400d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-07-23 14:36:33 +0300

    Handling POST requests using annotation @PostMapping

--- src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controller/DesignTacoController.java
+++ src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controller/DesignTacoController.java
@@ -6,10 +6,7 @@ import kz.zhelezyaka.springexperimentswithdeepdive.entity.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -57,6 +54,14 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco,
+                              @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(
