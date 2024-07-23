9309bd07931d910a7f05ed3aed0b4bb060bff256
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-07-23 15:57:57 +0300

    Order dispatch processing

--- src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controller/OrderController.java
+++ src/main/java/kz/zhelezyaka/springexperimentswithdeepdive/controller/OrderController.java
@@ -1,10 +1,13 @@
 package kz.zhelezyaka.springexperimentswithdeepdive.controller;
 
+import kz.zhelezyaka.springexperimentswithdeepdive.entity.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -16,4 +19,12 @@ public class OrderController {
     public String orderForm() {
         return "orderForm";
     }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
 }
+Order dispatch processing
